8ac7eb5de6a0f3e4c6523fc67d540b2d7e74da14
Paulo Jorge <103949580+thepaulojorge@users.noreply.github.com>
2025-10-08 09:57:10 -0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,7 @@
+/* ============================ typing animation =============================== */
+var typed = new Typed(".typing", {
+    strings:["","Desenvolvedor Front-End", "Desenvolvedor Web", "focado em criar experiências digitais incriveis"],
+    typeSpeed:150,
+    BackSpeed:120,
+    loop:true
+})
